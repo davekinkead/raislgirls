0c77b2975a18446ced605723178fa74fd9547e23
Dave Kinkead <dave@kinkead.com.au>
2013-11-16 17:12:03 +1000

    Initial commit;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jessica::Application.config.secret_key_base = '5b8e5a762bd96958dfcea312cf938660423f6d6eec17a6b8fae730d8ddb6e6d5a14f90b7a6b9ab877c90c104301c28daeb1e15491e368687bdcfe05fb9390650'
